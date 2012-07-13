19126859477e3cd93dc81d5b3e9fdbd121078038
Zachary Allaun <zach.allaun@gmail.com>
2012-07-13 16:54:15 -0400

    Implement simple client-server transfer semantics
    - reflect.clj: Create a simple endpoint for reflection api calls, with
      the ability to return compiled clojurescript as js.
    - reflect.cljs: Allow simple requests to be made to the server, with the
      result evaluated so as to become actual cljs data structures. Return
      this result in a primitive Var object.

--- src/cljs/clojure/reflect.cljs
+++ src/cljs/clojure/reflect.cljs
@@ -2,7 +2,22 @@
   (:require [clojure.browser.net :as net]
             [clojure.browser.event :as event]))
 
-(defn test-conn []
-  (let [conn (net/xhr-connection)]
-    (event/listen conn :success (fn [e] (.log js/console e)))
-    (net/transmit conn "/test")))
+(deftype Var [meta]
+  IMeta
+  (-meta [this] @meta))
+
+(defn evaluate-javascript [block]
+  (let [result (try (js* "eval(~{block})")
+                    (catch js/Error e
+                      (.log js/console e)))]
+    result))
+
+(defn var [sym]
+  (let [conn (net/xhr-connection)
+        sym-meta (atom :awaiting)]
+    (event/listen conn :success (fn [e]
+                                  (let [js (.getResponseText e/currentTarget ())]
+                                    (reset! sym-meta (evaluate-javascript js)))))
+    (event/listen conn :error #(reset! sym-meta :request-failed))
+    (net/transmit conn (str "/reflect?var=" (js/encodeURIComponent (str sym))))
+    (Var. sym-meta)))
